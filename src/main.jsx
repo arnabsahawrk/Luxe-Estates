cd49f732e6a49a86f548c17cd4af7b782bcbea14
arnabw <arnabsahawrk@gmail.com>
2024-04-09 07:08:15 +0600

    initial setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelmetProvider></HelmetProvider>
+  </React.StrictMode>
+);
