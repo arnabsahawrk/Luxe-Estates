1b016fe0b79c3573a677fe9859cc3f55b1e0efa2
arnabw <arnabsahawrk@gmail.com>
2024-04-10 08:48:45 +0600

    Animated on scroll added in How It Works Section

--- src/components/HowWorks/HowWorks.jsx
+++ src/components/HowWorks/HowWorks.jsx
@@ -2,14 +2,24 @@ import { CiDollar } from "react-icons/ci";
 import { RiHomeHeartLine } from "react-icons/ri";
 import { TbHomeSearch } from "react-icons/tb";
 
+//AOS
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
+
 const HowWorks = () => {
   return (
     <section className="text-center space-y-4 md:space-y-8">
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-[#5EA51D]">
         How It Works
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-3">
-        <div className="flex justify-center items-center flex-col space-y-2 px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-3 relative">
+        <div
+          data-aos="fade-right"
+          data-aos-duration="3000"
+          data-aos-delay="500"
+          className="flex justify-center items-center flex-col space-y-2 px-4"
+        >
           <TbHomeSearch className="text-4xl text-[#5EA51D] font-bold" />
           <h2 className="text-xl text-[#06112A] font-bold">Find Property.</h2>
           <p className="text-sm text-[#848093]">
@@ -19,7 +29,11 @@ const HowWorks = () => {
             needs.
           </p>
         </div>
-        <div className="flex justify-center items-center flex-col space-y-2 px-4">
+        <div
+          data-aos="zoom-in"
+          data-aos-duration="2000"
+          className="flex justify-center items-center flex-col space-y-2 px-4"
+        >
           <CiDollar className="text-4xl text-[#5EA51D] font-bold" />
           <h2 className="text-xl text-[#06112A] font-bold">Buy Property.</h2>
           <p className="text-sm text-[#848093]">
@@ -29,7 +43,12 @@ const HowWorks = () => {
             property hassle-free.
           </p>
         </div>
-        <div className="flex justify-center items-center flex-col md:col-span-2 lg:col-auto space-y-2 px-4">
+        <div
+          data-aos="fade-left"
+          data-aos-duration="3000"
+          data-aos-delay="1000"
+          className="flex justify-center items-center flex-col md:col-span-2 lg:col-auto space-y-2 px-4"
+        >
           <RiHomeHeartLine className="text-4xl text-[#5EA51D] font-bold" />
           <h2 className="text-xl text-[#06112A] font-bold">Make Investment.</h2>
           <p className="text-sm text-[#848093]">
